b8567f53858fbefec7e522d22dabb03cfd2aaf03
Meena Kannappan <meena.kanna.fm@gmail.com>
2024-09-04 15:23:45 +0000

    i'm done

--- notes.py
+++ notes.py
@@ -1,1 +1,10 @@
+#name = input ("What is your name?\n\t")
+#print ("Hi " + name)
 
+#print ("Hi " + name + " nice to meet you!")
+
+name = input ('Tell me your name!')
+email = input ('Tell me your email!')
+nickname = input ('Do you go by a nickname?')
+
+print ("Hey " + name + " or should I say " + nickname + "?")
